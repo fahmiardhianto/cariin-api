73f51a9af4c4f7761dd89b0371d766db554e7ec4
Fahmi Ardhianto <fahmi.ardhianto@gmail.com>
2014-06-04 14:04:53 +0700

    edit config for nginx and unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,15 @@
+root = "/home/fahmi/cariin-api"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.cariin-api.sock"
+worker_processes 2
+timeout 10
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
